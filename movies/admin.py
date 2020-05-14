d8111c77bf088d93bceaa1d542e30b60006c108b
Joe Cui <joe.cui@bpanz.com>
2020-05-14 23:05:59 +1000

    Added database data into admin page

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Genre, Movie
 
-# Register your models here.
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class MovieAdmin(admin.ModelAdmin):
+    # hide the date_created field from admin interface
+    exclude = ('date_created',)
+    # display attributes columns instead of an object
+    list_display = ('title', 'number_in_stock', 'daily_rate')
+
+# Register your models here, so that they will be shown in the admin interface
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    # SELECT * FROM movies_movie
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+    # # SELECT * FROM movies_movie WHERE rease_year=1985
+    # movies = Movie.objects.filter(rease_year=1985)
+    # # SELECT * FROM movies_movie WHERE id=1
+    # movies = Movie.objects.get(id=1)
+    return HttpResponse(output)
